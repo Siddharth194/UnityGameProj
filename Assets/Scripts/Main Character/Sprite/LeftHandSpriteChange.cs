a7799f17c65cbcd4fe4a889cb4c7cc5b6f8fb5f3
Siddharth194 <sidappu04@gmail.com>
2023-08-29 00:18:33 +0530

    second last commit

--- Assets/Scripts/Main Character/Sprite/LeftHandSpriteChange.cs
+++ Assets/Scripts/Main Character/Sprite/LeftHandSpriteChange.cs
@@ -7,21 +7,18 @@ public class LeftHandSpriteChange : MonoBehaviour
 {
     private List<UnityEngine.U2D.Animation.SpriteResolver> images;
     private bool attacking;
-    public int weapon = 0; // Changed the type to int to use it as an index
+    public int weapon = 0;
     public Animator anim;
 
-    // Start is called before the first frame update
     void Start() => images = new List<UnityEngine.U2D.Animation.SpriteResolver>(GetComponentsInChildren<UnityEngine.U2D.Animation.SpriteResolver>());
 
-    // Update is called once per frame
     void Update()
     {
-        // Removed 'private' from this line since it's already declared in the class
         attacking = anim.GetBool("Attacking");
 
         if (attacking)
         {
-            // Check if the weapon index is within bounds of the images list
+            
             if (weapon == 0 && weapon < images.Count)
             {
                 var item = images[weapon];
